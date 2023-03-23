eb279eb77b2fc2ac4359f14c733c51a4dc67117e
Octdecimal <572neet989@gmail.com>
2023-03-23 14:13:32 +0800

    add lab4

---
+++ lab4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void replace_all(char* str, char oldChar, char newChar) {
+	for(int i = 0; i < sizeof(str)/sizeof(char); i++) {
+		if(str[i] == oldChar) {
+			str[i] = newChar;
+		}
+	}
+}
+
+int main() {
+	char str[100], oldChar = 'a', newChar = 'A';
+	printf("Enter any string: ");
+	scanf("%s", str);
+
+	printf("\nString before replacing: \n%s", str);
+	replace_all(str, oldChar, newChar);
+	printf("\n\nString after replacing %c with %c : \n%s", oldChar, newChar, str);
+	
+	return 0;
+}
