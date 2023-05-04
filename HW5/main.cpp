bfbd3fb701521dec3d51faa1e87fa2412815cf1a
Octdecimal <572neet989@gmail.com>
2023-05-04 17:47:19 +0800

    modify HW5

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
-
 using namespace std;
-
 class TicTacToe{
 	public:
 	char list[10];
 	    void print_board(void){
 			cout << "     ▇     ▇     \n"\
 			    	"  "<<list[0]<<"  ▇  "<<list[1]<<"  ▇  "<<list[2]<<"   \n"\
-					"     ▇     ▇     \n"\
+				    "     ▇     ▇     \n"\
     				"▇▇▇▇▇▇▇▇▇▇\n"\
 	    			"     ▇     ▇     \n"\
 	    			"  "<<list[3]<<"  ▇  "<<list[4]<<"  ▇  "<<list[5]<<"   \n"\
 		    		"     ▇     ▇     \n"\
 			    	"▇▇▇▇▇▇▇▇▇▇\n"\
-				    "     ▇     ▇     \n"\
-   					"  "<<list[6]<<"  ▇  "<<list[7]<<"  ▇  "<<list[8]<<"   \n"\
+			    	"     ▇     ▇     \n"\
+   				    "  "<<list[6]<<"  ▇  "<<list[7]<<"  ▇  "<<list[8]<<"   \n"\
     				"     ▇     ▇     \n\n";
 		}
     	void run(void) {
@@ -28,17 +26,17 @@ class TicTacToe{
 	    		cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
 		    	print_board();
 		    	cout<<"Player "<<player<<", please input a number in range \"1~9\":  ";
-				while(cin >> input) {
-					if(list[input-1] < '1' or list[input-1] >'9') {
-						cout << "\nwrong input, please input a number: ";
-						cin >> input;
-					}else if(list[input-1]-'0' != input) {
-						cout << "\nthe number has been used, please input another number: ";
-						cin >> input;
-					}else{
-						break;
-					}
-				}
+		    	while(cin >> input) {
+			    	if(list[input-1] < '1' or list[input-1] >'9') {
+				    	cout << "\nwrong input, please input a number: ";
+					    cin >> input;
+				    }else if(list[input-1]-'0' != input) {
+					    cout << "\nthe number has been used, please input another number: ";
+					    cin >> input;
+	    			}else{
+		    			break;
+			    	}
+    			}
     			if(player == 1) {
 	    			list[input-1] = 'X';
 		    	} else{
@@ -46,27 +44,27 @@ class TicTacToe{
     			}
 	    		if(((list[0] == list[1]) and (list[1] == list[2])) or\
 		    	   ((list[3] == list[4]) and (list[4] == list[5])) or\
-			       ((list[6] == list[7]) and (list[7] == list[8])) or\
+	    		   ((list[6] == list[7]) and (list[7] == list[8])) or\
     			   ((list[0] == list[3]) and (list[3] == list[6])) or\
 	    		   ((list[1] == list[4]) and (list[4] == list[7])) or\
 		    	   ((list[2] == list[5]) and (list[5] == list[8])) or\
-			       ((list[0] == list[4]) and (list[4] == list[8])) or\
+		    	   ((list[0] == list[4]) and (list[4] == list[8])) or\
     			   ((list[2] == list[4]) and (list[4] == list[6]))) {
 	    			if(player == 1) {
-						cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
+					cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
 		    	        print_board();
 		    			cout << "==> Player 1 win";
 			    		return;
 				    }else {
-						cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
+					cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
 		    	        print_board();
     					cout << "==> Player 2 win";
 	    				return;
-		    		}
-			    }
-			    if(count == 8) {
-					cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
-		    	    print_board();
+			    	}
+	    		}
+		    	if(count == 8) {
+			    	cout << "\n\n-- Tic Tac Toe -- CSIE@CGU\nPlayer 1 (x)  -  Player 2 (O)\n\n";
+    				print_board();
     				cout << "==> Game draw";
 	    			return;
 		    	} else if(player==1){
