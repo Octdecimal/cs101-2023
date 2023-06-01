4dabb70df2d5d72d2ce1e0b08572b631b490d984
Mikado_Tokuteru <572neet989@gmail.com>
2023-06-01 14:53:14 +0800

    modify lab10

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -4,46 +4,46 @@
 using namespace std;
 
 class myString {
-		string m_str;
-	public:
-		myString(string s) {
-			m_str = s;
-		}
+    string m_str;
+public:
+    myString(string s) {
+        m_str = s;
+    }
 };
 
 class ReadClass{
-		string a = "class";
-		fstream in;
-		fstream jn;
-		string name = "main.cpp";
-	public:
-		void showClass() {
-			string str;
-			int count = 0;
-			in.open(name);
-			jn.open(name);
-			while(getline(jn,str)){
-				size_t pos = str.find(a);
-				if(pos != string::npos && pos == 0){
-					count++;
-				}
-			}
-			cout<<count << " class in main.cpp\n";
-			jn.close();
-			while(getline(in, str)){
-				string out;
-				size_t pos = str.find(a);
-				if(pos != string::npos && pos == 0) {
-					for(size_t i = pos + a.length() + 1; (str[i]>='a' && str[i]<='z')||(str[i]>='A' && str[i]<='Z')||(str[i]>='0' && str[i]<='9'||str[i]=='_'); i++) {
-						out += str[i]; 
-					}
-				}
-				if(!out.empty()){
-					cout << "class " << out << "\n";
-				}
-			}
-			in.close();
-		}
+    string a = "class";
+    fstream in;
+    fstream jn;
+    string name = "main.cpp";
+public:
+    void showClass() {
+        string str;
+        int count = 0;
+        in.open(name);
+        jn.open(name);
+        while(getline(jn,str)){
+            size_t pos = str.find(a);
+            if(pos != string::npos && pos == 0){
+                count++;
+            }
+        }
+        cout<<count << " class in main.cpp\n";
+        jn.close();
+        while(getline(in, str)){
+            string out;
+            size_t pos = str.find(a);
+            if(pos != string::npos && pos == 0) {
+                for(size_t i = pos + a.length() + 1; (str[i]>='a' && str[i]<='z')||(str[i]>='A' && str[i]<='Z')||(str[i]>='0' && str[i]<='9'||str[i]=='_'); i++) {
+                    out += str[i]; 
+                }
+            }
+            if(!out.empty()){
+                cout << "class " << out << "\n";
+            }
+        }
+        in.close();
+    }
 };
 
 int main() {
